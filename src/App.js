508a92c5050d9a3690e7441ee7fe31295b3065eb
ottercoder <ottercoder@gmail.com>
2020-05-26 23:32:29 +0200

    one more lint test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Java & Kotlin
+          Learn Java
         </a>
       </header>
     </div>
